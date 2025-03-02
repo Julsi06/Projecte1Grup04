55a704da0c9415606e2f4a8e60274683e929c023
Julsi06 <julia.sanchez@enti.cat>
2025-03-02 14:25:07 +0100

    Jump fet (cal modificar)

---
+++ Assets/scripts/PlayerJump.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D player;
+    private void Awake()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // If the player presses "space", the GameObject will jump
+        // The GameObject jumps for as long as the player presses "space" (arreglar)
+        if (Input.GetKey(KeyCode.Space))
+            player.velocity = new Vector2(player.velocity.x, speed);
+    }
+}
