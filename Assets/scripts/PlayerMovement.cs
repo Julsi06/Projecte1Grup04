a3d8f44787c05027f3a2cd5f94f389440257a26c
SoyTriggerU <unai.gonzalez@enti.cat>
2025-03-08 15:51:13 +0100

    Drag fixed

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,34 +6,37 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed; // S'estableix que la seva velocitat s'indicarà a Unity
+    [SerializeField] private float horizontalSpeed; // S'estableix que la seva velocitat s'indicarà a Unity
     private Rigidbody2D player; // S'estableix que tindrà físiques
         
     private void Awake()
     {
         // S'agafaran les dades del rigidbody2d del player a Unity
         player = GetComponent<Rigidbody2D>();
+        // Disable drag to avoid inertia
+        player.drag = 0;
     }
 
     //update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
+        // Captura la velocidad vertical actual para no afectar el salto
+        float VerticalVelocity = player.velocity.y;
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-            player.velocity = new Vector2((0-speed), 0);
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-            player.velocity = new Vector2(speed, 0);
-
-        float horizontalSpeed = speed;
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             player.velocity = new Vector2((0 - horizontalSpeed), 0);
         }
        
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             player.velocity = new Vector2(horizontalSpeed, 0);
         }
 
+        else
+        {
+            // Detiene solo el movimiento horizontal, mantiene el salto
+            player.velocity = new Vector2(0, VerticalVelocity);
+        }
     }
 }
