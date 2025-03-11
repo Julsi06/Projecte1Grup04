7248486462494e84e90fbc42ccd3029dc24052bf
SoyTriggerU <unai.gonzalez@enti.cat>
2025-03-11 13:14:03 +0100

    Revert "Fixed Errors"
    This reverts commit 24fd3ef75295ef87e9b6311ace26ebc7cdf2f47a.

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -40,6 +40,12 @@ public class PlayerMovement : MonoBehaviour
             horizontalVelocity = 0;
         }
 
+        else
+        {
+            // Detiene solo el movimiento horizontal (drag) cuando no se presionan teclas
+            horizontalVelocity = 0;
+        }
+        
         // Aplica la velocidad manteniendo el eje Y (vertical) sin cambios
         player.velocity = new Vector2(horizontalVelocity, verticalVelocity);
     }
