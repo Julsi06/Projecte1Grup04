e258661264e666c11ccac7b7ccde4bf264f36b2f
SoyTriggerU <unai.gonzalez@enti.cat>
2025-03-11 12:41:22 +0100

    Implemented Raycast on jump

--- Assets/scripts/PlayerJump.cs
+++ Assets/scripts/PlayerJump.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 
 public class PlayerJump : MonoBehaviour
 {
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private LayerMask groundLayer; // el layerMask estableix el que detectara el rayccast 
     [SerializeField] private int maxJumps = 2; // Número máximo de saltos permitidos
+    public float RaycastDistance = 0.5f;
+
 
     private Rigidbody2D player;
     private int jumpCount; // Contador de saltos
@@ -17,7 +19,7 @@ public class PlayerJump : MonoBehaviour
     {
         // Agafa els components del GameObject (jugador)
         player = GetComponent<Rigidbody2D>();
-        jumpCount = 0;
+        jumpCount = 1;
     }
 
     private void Update()
@@ -33,18 +35,15 @@ public class PlayerJump : MonoBehaviour
     private void CheckGrounded()
     {
         RaycastHit2D hit;
+        Vector2 raycastOrigin = transform.position - new Vector3(0f, 1.01f, 0f);
 
         // Lanza un rayo hacia abajo para detectar el suelo
-        hit = Physics2D.Linecast(transform.position + Vector3.down, transform.position + Vector3.down * 1.25f);
+        hit = Physics2D.Raycast(raycastOrigin, Vector2.down * 0.5f, RaycastDistance, groundLayer);
 
-
-        if (hit.collider != null)
+        // Si antes no estaba en el suelo pero ahora sí => Se reinicia el contador de saltos
+        if (hit.collider != null && hit.collider.CompareTag("Ground"))
         {
-            // Si antes no estaba en el suelo pero ahora sí => Se reinicia el contador de saltos
-            if (hit.collider.gameObject.tag == "Ground")
-            {
-                jumpCount = 0;
-            }
+            jumpCount = 1;
         }
     }
 
@@ -57,9 +56,8 @@ public class PlayerJump : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawSphere(transform.position + Vector3.down, 0.1f);
-        Gizmos.DrawSphere(transform.position + Vector3.down * 1.25f, 0.1f);
+        Vector2 raycastOrigin = transform.position - new Vector3(0f, 1.01f, 0f);
         // Dibuja el rayo en la escena para depuración
-        //Debug.DrawRay(transform.position + new Vector3(0, transform.position.y - 0.5f, 0), Vector3.down, Color.red);
+        Debug.DrawRay(raycastOrigin, Vector2.down*0.5f, Color.red);
     }
 }
