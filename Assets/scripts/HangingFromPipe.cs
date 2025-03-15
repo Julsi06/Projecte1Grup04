c4b36b8b36a912fec5cab2fba57b90c05f72df14
Julsi06 <julia.sanchez@enti.cat>
2025-03-15 16:08:54 +0100

    Beginning the layout for level 2

--- Assets/scripts/HangingFromPipe.cs
+++ Assets/scripts/HangingFromPipe.cs
@@ -9,14 +9,12 @@ public class HangingFromPipe : MonoBehaviour
     //[SerializeField] private float hangingSpeed;
 
     private Rigidbody2D player;
-    private bool isHanging = false;
 
     private void Awake()
     {
         player = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
         CheckHangingFromPipe();
@@ -26,17 +24,28 @@ public class HangingFromPipe : MonoBehaviour
     private void CheckHangingFromPipe()
     {
         Vector2 raycastOrigin = transform.position + new Vector3(0f, 1.01f);
-        isHanging = false;
 
         RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.up, 0.1f, pipeLayer);
         if (hit.collider != null && hit.collider.gameObject.name == "Pipe")
         {
+<<<<<<< HEAD
             isHanging = true;
         }
         if (isHanging)
         {
             player.gravityScale = -1f;
             // cal remodificar la gravetat quna no estigui tocant pipe
+=======
+            player.gravityScale = -1.0f;
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.gravityScale = 1.0f;
+            }
+        }
+        else
+        {
+            player.gravityScale = 1.0f;
+>>>>>>> 9b11068 (Beginning the layout for level 2)
         }
     }
 
