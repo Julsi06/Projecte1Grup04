3feb9d9e8b689ffc8cebb727944699b701bb2689
Julsi06 <julia.sanchez@enti.cat>
2025-03-24 12:25:26 +0100

    Shoot and camera following implemented in level 2
    By Júlia and Yumin

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -9,8 +9,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed; // S'estableix que la seva velocitat s'indicarà a Unity
     private Rigidbody2D player; // S'estableix que tindrà físiques
-    public HeartManager heartManager; // Relaciona el moviment del jugador amb el comptador de cors
-        
+    private bool isFacingRight = true; // Indica si el personaje mira a la derecha
+
     private void Awake()
     {
         // S'agafaran les dades del rigidbody2d del player a Unity
@@ -26,31 +26,45 @@ public class PlayerMovement : MonoBehaviour
         float verticalVelocity = player.velocity.y;
         float horizontalVelocity = horizontalSpeed;
 
+        //funcio de unity
+        float moveInput = Input.GetAxisRaw("Horizontal"); // -1 (izquierda), 0 (parado), 1 (derecha)
+
+
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             horizontalVelocity = -horizontalSpeed;
+
         }
-       
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             horizontalVelocity = horizontalSpeed;
-        }
 
+        }
         else
         {
             // Detiene solo el movimiento horizontal (drag) cuando no se presionan teclas
             horizontalVelocity = 0;
         }
-        
+
+        // Verifica si hay que cambiar la dirección del personaje
+        if (moveInput > 0 && !isFacingRight) // Se mueve a la derecha pero está mirando a la izquierda
+        {
+            flip();
+        }
+        else if (moveInput < 0 && isFacingRight) // Se mueve a la izquierda pero está mirando a la derecha
+        {
+            flip();
+        }
+
+
         // Aplica la velocidad manteniendo el eje Y (vertical) sin cambios
         player.velocity = new Vector2(horizontalVelocity, verticalVelocity);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void flip()
     {
-        if (collision.gameObject.CompareTag("heart"))
-        {
-            heartManager.heartCounter++;
-            Destroy(collision.gameObject);
-        }
+        isFacingRight = !isFacingRight; // Cambia la dirección
+                                        //viene de fabrica del unity (x,y,z);
+        transform.Rotate(0f, 180f, 0f);
     }
 }
