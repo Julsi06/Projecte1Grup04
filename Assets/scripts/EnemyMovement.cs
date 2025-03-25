c4f4426d0615dd3387fc910e7d230da5dabfbbb1
Yumin Martinez Perez <yumin.martinez@enti.cat>
2025-03-25 23:29:18 +0100

    enemymovement

--- Assets/scripts/EnemyMovement.cs
+++ Assets/scripts/EnemyMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform player; // Indica la posición del player
+    public float detectionRadius = 5.0f;
+    public float speed = 15.0f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        // Si el jugador está dentro del radio de detección
+        if (distanceToPlayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            movement = new Vector2(direction.x, 0); // Se mueve solo en X
+        }
+        else
+        {
+            movement = Vector2.zero; // No se mueve
+        }
+    }
+
+    // Mover el enemigo en FixedUpdate para que funcione bien con la física
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
