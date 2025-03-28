b579f237849d3ac087382c6cf7983636e39cec11
Julsi06 <julia.sanchez@enti.cat>
2025-03-28 18:11:49 +0100

    Adding more sprites

---
+++ Assets/scripts/hangingFromPipe.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hangingFromPipe : MonoBehaviour
+{
+    [SerializeField] private LayerMask pipeLayer;
+
+    private Rigidbody2D player;
+
+    private void Awake()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        CheckHangingFromPipe();
+    }
+
+    private void CheckHangingFromPipe()
+    {
+        Vector2 raycastOrigin = transform.position + new Vector3(0f, 1.05f);
+
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.up, 0.5f, pipeLayer);
+        if (hit.collider != null && hit.collider.gameObject.CompareTag("Pipe"))
+        {
+            player.gravityScale = -1.0f;
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.gravityScale = 1.0f;
+            }
+        }
+        else
+        {
+            player.gravityScale = 1.0f;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector2 raycastOrigin = transform.position + new Vector3(0f, 1.5f);
+        Debug.DrawRay(raycastOrigin, Vector2.up * 0.5f, Color.red);
+    }
+
+}
