7cdc0710505ac060009e3d961984fc1d66e4a1a6
Yumin Martinez <yumin.martinez@enti.cat>
2025-03-31 13:29:12 +0200

    plataforma
    se cae cuando el jugador se sube

---
+++ Assets/scripts/plataformasTemporal.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformasTemporal : MonoBehaviour
+{
+    // Tiempo que esperará la plataforma antes de caer
+    [SerializeField] private float waitTime;
+
+    // Referencia al componente Rigidbody2D para controlar la física
+    private Rigidbody2D rb2D;
+
+    // Velocidad de rotación (no está siendo utilizada en este código)
+    [SerializeField] private float velocidadRotacion;
+
+    // Variable booleana para indicar si la plataforma está en caída (no se usa actualmente)
+    private bool caida = false;
+
+    private void Start()
+    {
+        // Obtiene el Rigidbody2D del objeto al inicio
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Se ejecuta cuando la plataforma colisiona con otro objeto
+    private void OnCollision2D(Collision2D other)
+    {
+        // Verifica si el objeto con el que colisiona tiene la etiqueta "Player"
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Inicia la corutina que hará caer la plataforma después de esperar un tiempo
+            StartCoroutine(Caida(other));
+        }
+    }
+
+    private IEnumerator Caida(Collision2D other)
+    {
+        // Espera la cantidad de segundos especificada en waitTime antes de ejecutar el resto del código
+        yield return new WaitForSeconds(waitTime); // FUNCION DE UNITY
+
+        // Ignora la colisión entre la plataforma y el jugador, evitando interacciones físicas no deseadas
+        Physics2D.IgnoreCollision(transform.GetComponent<Collider2D>(), other.gameObject.GetComponent<Collider2D>());
+
+        // Quita todas las restricciones del Rigidbody2D para permitir el movimiento y la rotación libre
+        rb2D.constraints = RigidbodyConstraints2D.None;
+
+        // Aplica una pequeña fuerza en la dirección X para iniciar el movimiento de caída
+        rb2D.AddForce(new Vector2(0.1f, 0));
+
+        // Aquí la plataforma comenzará a caer
+    }
+}
