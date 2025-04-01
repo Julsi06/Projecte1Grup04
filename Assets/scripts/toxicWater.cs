1f44f5fdb0219ef6f26d6057b32af9d690284900
Julsi06 <julia.sanchez@enti.cat>
2025-04-01 13:14:02 +0200

    jkj

--- Assets/scripts/toxicWater.cs
+++ Assets/scripts/toxicWater.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class toxicWater : MonoBehaviour
+public class ToxicWater : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("toxicWater"))
+        if (collision.gameObject.CompareTag("ToxicWater"))
         {
             Death();
         }
     }
+
     void Death()
     {
         gameObject.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-
 }
