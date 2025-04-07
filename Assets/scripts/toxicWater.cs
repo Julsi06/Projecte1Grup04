7172eac64bf93972aeaee5ec04ddbfb2094b9726
SoyTriggerU <unai.gonzalez@enti.cat>
2025-04-07 14:00:53 +0200

    started implementing tilemap

--- Assets/scripts/toxicWater.cs
+++ Assets/scripts/toxicWater.cs
@@ -7,7 +7,7 @@ public class ToxicWater : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("ToxicWater"))
+        if (collision.gameObject.CompareTag("toxicWater"))
         {
             Death();
         }
