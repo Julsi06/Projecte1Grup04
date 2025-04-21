3b6f857dc08a59803c02952342f4f2c13e93e5ba
Julsi06 <julia.sanchez@enti.cat>
2025-04-21 17:29:16 +0200

    Falling platforms done
    still needs tilemap and player's animations

---
+++ Assets/scripts/FallingPlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    // Time parameters for platforms
+    private float fallDelay = 0.5f;
+    private float destroyDelay = 2f;
+
+    // Giving the platform a rigidbody so we can manipulate its states
+    [SerializeField] private Rigidbody2D platform;
+
+    // Checks for collision
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+            StartCoroutine(Fall());
+    }
+
+    // Used to control the time and the platform's states
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        // Transforms platform's rigidbody from kinematic to dynamic
+        platform.bodyType = RigidbodyType2D.Dynamic;
+        // Will destroy the object in a given time
+        Destroy(gameObject, destroyDelay);
+    }
+}
