df3537dea00f8a813697c4c106f3d4313547dfde
Julsi06 <julia.sanchez@enti.cat>
2025-04-22 11:53:42 +0200

    jump needs fixing

--- Assets/scripts/LevelRelated/ToxicWater.cs
+++ Assets/scripts/LevelRelated/ToxicWater.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class ToxicWater : MonoBehaviour
 {
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("toxicWater"))
+            Death();
+    }
 
+    private void Death()
+    {
+        gameObject.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
