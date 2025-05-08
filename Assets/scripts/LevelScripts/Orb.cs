854cb374935ce2adb4229b72f9f42afebb40a69d
SoyTriggerU <unai.gonzalez@enti.cat>
2025-05-08 11:43:30 +0200

    Assets inlcuded, scripts adapted and almost functional gameplay
    Only button/door script miss

--- Assets/scripts/LevelScripts/Orb.cs
+++ Assets/scripts/LevelScripts/Orb.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Orb : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             Destroy(gameObject);
         }
     }
 }
+
