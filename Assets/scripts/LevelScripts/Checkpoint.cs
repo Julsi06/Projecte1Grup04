f2fd753c0e6d7daff418fb885a190ed33da34eda
SoyTriggerU <unai.gonzalez@enti.cat>
2025-05-11 13:38:23 +0200

    Beta version ready
    Only some minor bugs left

--- Assets/scripts/LevelScripts/Checkpoint.cs
+++ Assets/scripts/LevelScripts/Checkpoint.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Chechkpoint : MonoBehaviour
 {
+    private Animator animator;
+    private bool isActivated = false;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            isActivated = true;
             SpawnManager.Instance.SetSpawnPoint(transform.position);
             Debug.Log("Checkpoint reached on: " + transform.position);
+            animator.SetTrigger("Activated");
         }
     }
 }
