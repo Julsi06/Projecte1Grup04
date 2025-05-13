e7d10f256f87de302235d32569c4e671eaad728f
Julsi06 <julia.sanchez@enti.cat>
2025-05-13 12:49:35 +0200

    player attaches to moving platforms

---
+++ Assets/scripts/LevelScripts/AttachPlayerPlatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachPlayerPlatform : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
+
