fbf013080a395017fda2af73f0cad291d63a4e0c
Yumin Martinez Perez <yumin.martinez@enti.cat>
2025-05-13 23:12:43 +0200

    arreglado unas cosas

--- Assets/scripts/PlayerScripts/weapon.cs
+++ Assets/scripts/PlayerScripts/weapon.cs
@@ -6,14 +6,38 @@ public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+    [SerializeField] private int bulletLoad = 3;
+    private int actualBullet;
+    public GameObject bulletUI;
+    public GameObject bulletUI1;
+    public GameObject bulletUI2;
 
-    void Update()
+
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        actualBullet = bulletLoad;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && actualBullet > 0)
         {
             Debug.Log("Tecla Q presionada"); // Mensaje de prueba
             Shoot();
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            reload();
+        }
+    }
+
+
+    void reload()
+    {
+        actualBullet = bulletLoad;
+        bulletUI.SetActive(true);
+        bulletUI1.SetActive(true);
+        bulletUI2.SetActive(true);
     }
 
     void Shoot()
@@ -32,6 +56,18 @@ public class Weapon : MonoBehaviour
 
         Debug.Log("Disparando...");
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        actualBullet--;
+
+        // Oculta las balas según las que queden
+        if (actualBullet == 2)
+            bulletUI2.SetActive(false);
+        else if (actualBullet == 1)
+            bulletUI1.SetActive(false);
+        else if (actualBullet == 0)
+            bulletUI.SetActive(false);
     }
+
+
 }
+///////
 
