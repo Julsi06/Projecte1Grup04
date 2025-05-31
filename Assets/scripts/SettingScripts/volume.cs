03cda5dd691d3e0436e4fd0890e6ce292f95a32c
Samuel CohenEnti <samuel.cohen@enti.cat>
2025-05-31 13:48:30 +0200

    musica ficada amb logaritmes

--- Assets/scripts/SettingScripts/volume.cs
+++ Assets/scripts/SettingScripts/volume.cs
@@ -1,43 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
-
-/*
 public class Volume : MonoBehaviour
 {
     public Slider slider;
     public float sliderValue;
-    public Image imagenMute; // Corregido el nombre y tipo de variable
+    public Image imagenMute;
+    private AudioMixer mixer; // Ya no es público, se carga automáticamente
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f); // Corregido "PlayerPrefers" a "PlayerPrefs"
-        sliderValue = slider.value; // Asegurar que sliderValue esté sincronizado
-        AudioListener.volume = slider.value;
-        RevisarSiEstoyMute(); // Corregido el nombre del método para seguir convenciones
+        // Cargar el AudioMixer desde Resources/MainMixer.mixer
+        mixer = Resources.Load<AudioMixer>("MainMixer");
+
+        if (mixer == null)
+        {
+            Debug.LogError("No se pudo cargar el AudioMixer. Asegúrate de que 'MainMixer.mixer' esté en la carpeta Resources.");
+        }
     }
 
-    public void ChangeSlider(float value) // Método faltante para manejar cambios en el slider
+    void Start()
     {
-        sliderValue = value;
-        PlayerPrefs.SetFloat("volumenAudio", sliderValue);
-        AudioListener.volume = sliderValue;
+        slider.value = PlayerPrefs.GetFloat("volumenAudio", 0.5f);
+        sliderValue = slider.value;
+        SetVolume(sliderValue);
         RevisarSiEstoyMute();
     }
 
-    public void RevisarSiEstoyMute() // Corregido el nombre del método
+    public void ChangeSlider(float value)
     {
-        if (sliderValue == 0)
-        {
-            imagenMute.enabled = true;
-        }
-        else
-        {
-            imagenMute.enabled = false;
-        }
+        sliderValue = value;
+        PlayerPrefs.SetFloat("volumenAudio", sliderValue);
+        SetVolume(sliderValue);
+        RevisarSiEstoyMute();
+    }
+
+    private void SetVolume(float value)
+    {
+        float dB = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20f;
+        mixer.SetFloat("VolumenMaster", dB);
+    }
+
+    public void RevisarSiEstoyMute()
+    {
+        imagenMute.enabled = sliderValue <= 0.0001f;
     }
 }
-*/
